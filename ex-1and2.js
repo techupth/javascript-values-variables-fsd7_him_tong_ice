c79e214b03e698a16b6dbddf7b8c871d53fb1efc
Eisss20 <pajenan.j@gmail.com>
2024-09-30 13:57:17 +0700

    Feat:Answer Assignment JS 1

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "Karter";
+let surName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
 
-//Exercise #2: Variable Reassignment
-//Start coding here
+const restaurant = "Chef's Table by Chef James";
+const netFoodPrice = 15000;
+const activeRestaurant = 1500;
+
+address = "970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+netFoodPrice = 5000; // เป็น const ไม่สามารถแสดงได้
+activeReataurant = 2500; // เป็น const ไม่สามารถแสดงได้
+
+
