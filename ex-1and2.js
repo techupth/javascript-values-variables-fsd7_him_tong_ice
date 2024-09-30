57dbf26381199c66b8a1ebed9debef6b4cb1c334
tong <reangsak1234@hotmail.com>
2024-09-30 13:34:47 +0700

    feat: JS value variable assignment

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let name = "Karter";
+let surname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const priceOnBill = 15000;
+const openRestaurant = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+priceOnBill = 5000;
+openRestaurant = 2500;
